a408d3f2fba5e7f30324200dd66ac4fd346f82f1
ohichii <ohichi.hmza@gmail.com>
2016-02-06 20:07:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/azmah/treollastepstest/TableJasonFiles.java
@@ -0,0 +1,12 @@
+package com.example.azmah.treollastepstest;
+
+/**
+ * Created by azmah on 05/02/2016.
+ */
+public class TableJasonFiles {
+    public TableJasonFiles(){}
+
+    public static abstract class TableInfo{
+
+    }
+}
